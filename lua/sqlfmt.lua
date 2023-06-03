d2a9cfaadd5a53b051cf74447f1cd879ade7160d
Ryder <ddzero2c@gmail.com>
2023-06-04 02:10:04 +0800

    fix: call sql-formatter

--- lua/sqlfmt.lua
+++ lua/sqlfmt.lua
@@ -3,15 +3,15 @@ local M = {}
 local embedded_sql = vim.treesitter.query.parse(
     "go",
     [[
-                (call_expression
-                  (selector_expression
-                    operand: (identifier) @operand
-                    field: (field_identifier) @field (#contains? @field "QueryRow" "Exec" "Query" ))
-                  (argument_list
-                    (raw_string_literal) @sql
-                )
-                )
-        ]]
+        (call_expression
+          (selector_expression
+            field: (field_identifier) @field (#eq? @field "QueryContext")
+          )
+          (argument_list
+            (raw_string_literal)@sql_string
+          )
+        )
+    ]]
 )
 
 local get_root = function(bufnr)
@@ -21,30 +21,15 @@ local get_root = function(bufnr)
 end
 
 local function formatter(str)
-    str = str:gsub("'", "\\'")
-    local cmd = "node -e \"const sqlFormatter = require('sql-formatter'); console.log(sqlFormatter.format('" ..
-        str .. "'))\""
-    local output = {}
-    local job = vim.fn.jobstart(cmd, {
-        stdout_buffered = true,
-        on_stdout = function(_, data)
-            if data then
-                output = data
-            end
-        end,
-        on_stderr = function(_, data)
-            local err = table.concat(data)
-            if data and err ~= "" then
-                vim.notify("Improper sql syntax:", err)
-            end
-        end,
-    })
-    vim.fn.jobwait({ job })
-    return output
+    -- str = str:gsub("`", "'")
+    local handle = io.popen("echo '" .. str .. "' | sql-formatter -l postgresql")
+    local result = handle:read("*a")
+    handle:close()
+    return result
 end
 
 function M.format_sql()
-    bufnr = bufnr or vim.api.nvim_get_current_buf()
+    local bufnr = vim.api.nvim_get_current_buf()
 
     if vim.bo[bufnr].filetype ~= "go" then
         vim.notify("can only be used in go")
@@ -52,25 +37,21 @@ function M.format_sql()
     end
 
     local root = get_root(bufnr)
-
-    local changes = {}
     for id, node in embedded_sql:iter_captures(root, bufnr, 0, -1) do
         local name = embedded_sql.captures[id]
-        if name == "sql" then
+        if name == "sql_string" then
             local range = { node:range() }
-            local indentation = "    "
+            local sql_string = vim.treesitter.get_node_text(node, bufnr):sub(2, -2)
 
-            local formatted = formatter(vim.treesitter.get_node_text(node, bufnr))
-
-            for idx, line in ipairs(formatted) do
-                formatted[idx] = indentation .. line
+            local formatted = formatter(sql_string)
+            local lines = {}
+            for s in string.gmatch(formatted, "[^\n]+") do
+                table.insert(lines, s)
             end
 
-            table.insert(changes, 1, { start = range[1] + 1, final = range[3], formatted = formatted })
+            vim.api.nvim_buf_set_lines(bufnr, range[1] + 1, range[3], false, lines)
         end
     end
-
-    for _, change in ipairs(changes) do
-        vim.api.nvim_buf_set_lines(bufnr, change.start, change.final, false, change.formatted)
-    end
 end
+
+return M
