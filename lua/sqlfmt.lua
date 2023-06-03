c844be006da1092e5a2325f7c0d226248b93a176
Ryder <ddzero2c@gmail.com>
2023-06-03 23:15:23 +0800

    fix: use treesitter query

--- lua/sqlfmt.lua
+++ lua/sqlfmt.lua
@@ -1,60 +1,76 @@
-local api = vim.api
-local ts_utils = require 'nvim-treesitter.ts_utils'
-
 local M = {}
 
-local function is_sql_string(node)
-    local src = ts_utils.get_node_text(node)[1]
-    return src:find('SELECT') or src:find('INSERT') or
-        src:find('UPDATE') or src:find('DELETE')
+local embedded_sql = vim.treesitter.query.parse(
+    "go",
+    [[
+                (call_expression
+                  (selector_expression
+                    operand: (identifier) @operand
+                    field: (field_identifier) @field (#contains? @field "QueryRow" "Exec" "Query" ))
+                  (argument_list
+                    (raw_string_literal) @sql
+                )
+                )
+        ]]
+)
+
+local get_root = function(bufnr)
+    local parser = vim.treesitter.get_parser(bufnr, "go", {})
+    local tree = parser:parse()[1]
+    return tree:root()
 end
 
-local function traverse_tree(node, action)
-    if node:type() == 'raw_string_lit' then
-        action(node)
-    end
-    for child in node:iter_children() do
-        traverse_tree(child, action)
-    end
+local function formatter(str)
+    str = str:gsub("'", "\\'")
+    local cmd = "node -e \"const sqlFormatter = require('sql-formatter'); console.log(sqlFormatter.format('" ..
+        str .. "'))\""
+    local output = {}
+    local job = vim.fn.jobstart(cmd, {
+        stdout_buffered = true,
+        on_stdout = function(_, data)
+            if data then
+                output = data
+            end
+        end,
+        on_stderr = function(_, data)
+            local err = table.concat(data)
+            if data and err ~= "" then
+                vim.notify("Improper sql syntax:", err)
+            end
+        end,
+    })
+    vim.fn.jobwait({ job })
+    return output
 end
 
 function M.format_sql()
-    local bufnr = api.nvim_get_current_buf()
-    local ft = api.nvim_buf_get_option(bufnr, 'filetype')
-    local parser = vim.treesitter.get_parser(bufnr, ft)
+    bufnr = bufnr or vim.api.nvim_get_current_buf()
 
-    -- Read configuration options from sqlformat.json.
-    local options = {}
-    local f = io.open('sqlformat.json', 'r')
-    if f then
-        options = vim.fn.json_decode(f:read('*all'))
-        f:close()
+    if vim.bo[bufnr].filetype ~= "go" then
+        vim.notify("can only be used in go")
+        return
     end
 
-    -- Convert options to a string that can be passed to Node.js.
-    local options_str = vim.fn.json_encode(options):gsub('"', '\\"')
+    local root = get_root(bufnr)
 
-    parser:for_each_tree(function(tstree)
-        traverse_tree(tstree:root(), function(node)
-            if is_sql_string(node) then
-                local sql_string = ts_utils.get_node_text(node)[1]
-                sql_string = sql_string:sub(2, -2)
-                -- Pass options to sql-formatter.
-                local formatted_sql = vim.fn.system(string.format(
-                    'node -e "const sqlFormatter = require(\'sql-formatter\'); console.log(sqlFormatter.format(\'%s\', %s))"',
-                    sql_string, options_str))
+    local changes = {}
+    for id, node in embedded_sql:iter_captures(root, bufnr, 0, -1) do
+        local name = embedded_sql.captures[id]
+        if name == "sql" then
+            local range = { node:range() }
+            local indentation = "    "
 
-                -- get the start and end row of the current node
-                local start_row, _, end_row, _ = node:range()
+            local formatted = formatter(vim.treesitter.get_node_text(node, bufnr))
 
-                -- split the formatted SQL into lines
-                local formatted_lines = vim.split(formatted_sql, '\n', true)
-
-                -- update the lines in the buffer
-                api.nvim_buf_set_lines(bufnr, start_row, end_row + 1, false, formatted_lines)
+            for idx, line in ipairs(formatted) do
+                formatted[idx] = indentation .. line
             end
-        end)
-    end)
-end
 
-return M
+            table.insert(changes, 1, { start = range[1] + 1, final = range[3], formatted = formatted })
+        end
+    end
+
+    for _, change in ipairs(changes) do
+        vim.api.nvim_buf_set_lines(bufnr, change.start, change.final, false, change.formatted)
+    end
+end
