4a602c364e2de43911dd2bf14244a5467bdbcd9e
Ryder <ddzero2c@gmail.com>
2023-06-04 13:24:37 +0800

    feat: aspect config

--- lua/sqlfmt.lua
+++ lua/sqlfmt.lua
@@ -5,10 +5,11 @@ local embedded_sql = vim.treesitter.query.parse(
     [[
         (call_expression
           (selector_expression
-            field: (field_identifier) @field (#eq? @field "QueryContext")
+            field: (field_identifier) @field
+            (#match? @field "^(Query|QueryContext|Exec|ExecContext|Prepare|PrepareContext)$")
           )
           (argument_list
-            (raw_string_literal)@sql_string
+            (raw_string_literal) @sql
           )
         )
     ]]
@@ -21,10 +22,23 @@ local get_root = function(bufnr)
 end
 
 local function formatter(str)
-    -- str = str:gsub("`", "'")
-    local handle = io.popen("echo '" .. str .. "' | sql-formatter -l postgresql")
+    -- check for local config
+    local config_file_path = vim.fn.getcwd() .. "/.sqlformat"
+
+    -- If the file exists, add the --config flag to the command
+    local config = ""
+    if vim.fn.filereadable(config_file_path) == 1 then
+        config = "--config " .. config_file_path
+    end
+
+    local handle = io.popen("echo '" .. str .. "' | sql-formatter " .. config)
     local result = handle:read("*a")
-    handle:close()
+    local success, _, code = handle:close()
+    if not success then
+        vim.notify("Failed to format SQL. Exit code: " .. tostring(code), vim.log.levels.ERROR)
+        return str
+    end
+
     return result
 end
 
@@ -39,11 +53,16 @@ function M.format_sql()
     local root = get_root(bufnr)
     for id, node in embedded_sql:iter_captures(root, bufnr, 0, -1) do
         local name = embedded_sql.captures[id]
-        if name == "sql_string" then
+        if name == "sql" then
             local range = { node:range() }
             local sql_string = vim.treesitter.get_node_text(node, bufnr):sub(2, -2)
 
             local formatted = formatter(sql_string)
+            if sql_string == formatted then
+                -- Skip this node if the SQL didn't change
+                goto continue
+            end
+
             local lines = {}
             for s in string.gmatch(formatted, "[^\n]+") do
                 table.insert(lines, s)
@@ -51,6 +70,7 @@ function M.format_sql()
 
             vim.api.nvim_buf_set_lines(bufnr, range[1] + 1, range[3], false, lines)
         end
+        ::continue::
     end
 end
 
